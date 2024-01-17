87b225274f2f55fa97876ef9a60c8684da5cbc91
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2024-01-17 18:13:31 +0100

    main3.py kesz, osztalyos felepitesben, szines konzollal

---
+++ tablastring.py
@@ -0,0 +1,12 @@
+TABLASTRING = """
+              Gép   Játékos
+Tetszőleges   {gt}         {jt}
+Pár           {gp}         {jp}
+Drill         {gd}         {jd}
+Két pár       {gkp}         {jkp}
+Kis póker     {gkip}         {jkip}
+Full          {gf}         {jf}
+Kis sor       {gks}         {jks}
+Nagy sor      {gns}         {jns}
+Nagy póker    {gnp}         {jnp}
+"""
